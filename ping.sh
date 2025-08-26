6afb6e175c8b7550118b1af7d31e866cc9273522
Design graphic <121799311+Alidjrif@users.noreply.github.com>
2025-08-26 14:38:58 +0300

    Create ping.sh

---
+++ ping.sh
@@ -0,0 +1,20 @@
+#!/data/data/com.termux/files/usr/bin/bash
+
+GREEN='\033[0;32m'
+RED='\033[0;31m'
+NC='\033[0m' # Reset
+
+read -p "enter ip :" ip
+
+while true; do
+    ping_result=$(ping -c 1 -W 1 $ip | grep 'time=')
+
+    if [[ $ping_result == *"time="* ]]; then
+        time_ms=$(echo $ping_result | grep -oP 'time=\>
+        echo -ne "\r$ip --> ${GREEN}${time_ms} ms${NC}>
+    else
+        echo -ne "\r$ip --> ${RED}❌ The device is not>
+    fi
+
+    sleep 0.5
+done
